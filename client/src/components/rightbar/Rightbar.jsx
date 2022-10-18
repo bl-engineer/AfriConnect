1de8523866b04a8885bed241ed24ac2cd2f80eb5
Bocar Ly <bl-engineer@Bocars-iMac.attlocal.net>
2022-10-18 09:55:13 -0400

    updated components and api fetching

--- client/src/components/rightbar/Rightbar.jsx
+++ client/src/components/rightbar/Rightbar.jsx
@@ -1,5 +1,5 @@
 import "./rightbar.css";
-import { Users } from "../../dummyData";
+import { Users } from "../../seed";
 import Online from "../online/Online";
 import { useContext, useEffect, useState } from "react";
 import axios from "axios";

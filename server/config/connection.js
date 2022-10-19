f5cf1bb3d5bdd6e0f639e6b2eaab0a03c0f7a8f6
Bocar Ly <bl-engineer@Bocars-iMac.attlocal.net>
2022-10-19 08:47:56 -0400

    fixing connection issue

---
+++ server/config/connection.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(
+  process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/Africonnect_DB',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+module.exports = mongoose.connection;

7da890d87e04f49ae0eb2d5012ee2fd0b8eafe15
Bocar Ly <bl-engineer@Bocars-iMac.attlocal.net>
2022-10-19 22:37:14 -0400

    Final version

--- client/src/App.js
+++ client/src/App.js
@@ -6,22 +6,21 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Redirect,
 } from "react-router-dom";
-import { useContext } from "react";
-import { AuthContext } from "./context/AuthContext";
+// import { useContext } from "react";
+// import { AuthContext } from "./context/AuthContext";
 
 function App() {
-  const { user } = useContext(AuthContext);
+  // const { user } = useContext(AuthContext);
   return (
     <Router>
       <Switch>
         <Route exact path="/">
-          {user ? <Home /> : <Register />}
+          <Home />
         </Route>
-        <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
+        <Route path="/login"><Login /></Route>
         <Route path="/register">
-          {user ? <Redirect to="/" /> : <Register />}
+           <Register />
         </Route>
         <Route path="/profile/:username">
           <Profile />
